14e33c89bd1bd1b10bf16a98bd33416eecbdd5de
xpaul <xpaulz@yahoo.com>
2014-04-04 01:25:44 -0700

    update for push_local.sh and smacker1 hostroot

--- push_local.sh
+++ push_local.sh
@@ -4,14 +4,15 @@
 # needs `which tar`
 GITROOT=~/git/https/github.com/xpaulz/workstations
 HOSTROOT=$GITROOT/`hostname`
-test -d $HOSTROOT || mkdir -p $HOSTROOT
 
-pushd $GITROOT
-test -d .git || { pushd ../; git clone https://github.com/xpaulz/workstations ; popd; }
+pushd $GITROOT || {  mkdir -p $GITROOT ;  pushd $GITROOT ; }
+test -d .git || { pushd ../; mv workstations{,.`date +%Y%m%d.%H%M`} ; git clone https://github.com/xpaulz/workstations ; popd; }
 git pull
 
-test -d $HOSTROOT || mkdir -p $HOSTROOT
+pushd $HOSTROOT || { mkdir -p $HOSTROOT; pushd $HOSTROOT; }
 
 tar czf -  ~/.aws ~/.ssh ~/.gnupg ~/.gdfuse/*/config ~/.awssecret ~/.bash* | gpg -r xpaulz@gmail.com -r xpaul@spokeo.com -a -e > $GITROOT/`hostname`/keys.tgz.asc
-git commit -a -m "updating keys from pvigils-lx1" 
+ls -l keys.tgz.asc
+
+git commit -a -m "updating keys from `hostname`" 
 git push
