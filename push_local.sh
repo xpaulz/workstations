8acc84491a70fb36b31a96ea875d331300126f9f
Paul Vigil <pvigil@spokeo.com>
2014-04-04 01:07:29 -0700

    updating keys from pvigils-lx1

---
+++ push_local.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# needs `which git`
+# needs `which tar`
+# needs `which tar`
+GITROOT=~/git/https/github.com/xpaulz/workstations
+HOSTROOT=$GITROOT/`hostname`
+test -d $HOSTROOT || mkdir -p $HOSTROOT
+
+pushd $GITROOT
+test -d .git || { pushd ../; git clone https://github.com/xpaulz/workstations ; popd; }
+git pull
+
+test -d $HOSTROOT || mkdir -p $HOSTROOT
+
+tar czf -  ~/.aws ~/.ssh ~/.gnupg ~/.gdfuse ~/.awssecret ~/.bash* | gpg -r xpaulz@gmail.com -r xpaul@spokeo.com -a -e > $GITROOT/`hostname`/keys.tgz.asc
+git commit -a -m "updating keys from pvigils-lx1" 
+git push
